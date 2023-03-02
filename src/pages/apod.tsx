13e45e3a7198a4a2224e56c8519643afd79ef3d3
yousaf wazir <umbrellaman612@gmail.com>
2023-03-02 14:07:07 +0000

    added skelton for image

--- src/pages/apod.tsx
+++ src/pages/apod.tsx
@@ -1,3 +1,8 @@
 export default function Page() {
-  return <section>APOD</section>;
+  return (
+    <section className="h-[35rem] md:h-[45rem] relative border">
+      {/* Image */}
+      <div className="w-3/4 h-3/4 md:w-1/2 md:h-1/2 border border-black mx-auto absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">APOD</div>
+    </section>
+  );
 }
